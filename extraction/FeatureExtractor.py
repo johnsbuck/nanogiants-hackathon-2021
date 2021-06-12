57fd789ee2c3933650ca07117ee3e4885d4756e8
Nick LaPosta <lapost48@gmail.com>
2021-06-12 03:53:20 -0400

    Displays wordcloud of most relevant words

--- extraction/FeatureExtractor.py
+++ extraction/FeatureExtractor.py
@@ -1,23 +1,18 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-from  scipy.sparse import find
+from scipy.sparse import find
 import json
 import numpy as np
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+from collections import Counter
 
-def extract_features(review_file="../data/google/user_reviews.json", num_features=5):
-    with open(review_file) as json_reviews:
-        user_reviews = json.load(json_reviews)
 
-    review_words = []
-    for review in user_reviews[1:101]:
-        review_words.append(" ".join(review[1]))
-
+def extract_features(review_words, num_features=5):
     vectorizer = TfidfVectorizer()
 
     vectors = vectorizer.fit_transform(review_words)
 
     feature_names = np.asarray(vectorizer.get_feature_names())
-
-    dense = vectors.todense()
 
     # print(len(feature_names))
     #
@@ -29,8 +24,48 @@ def extract_features(review_file="../data/google/user_reviews.json", num_feature
     for i in range(len(doc)):
         tfidf_values.append([vectors[doc[i], feature[i]], feature_names[feature[i]]])
 
-    sorted_values = sorted(tfidf_values, key=lambda val: val[0])
-    print(sorted_values)
+    sorted_values = sorted(tfidf_values, key=lambda val: val[0], reverse=True)
 
-    # print(list(zip(np.matrix.tolist(vectors[doc, feature]), feature_names[feature])))
+    all_keywords = []
+    for i in range(len(sorted_values[:len(sorted_values) // 4])):
+        all_keywords.append(sorted_values[i][1])
 
+    counter = Counter()
+    counter.update(all_keywords)
+
+    print(len(counter.keys()))
+
+    count = 0
+    words_added = 0
+    cut_top = 10
+    cut_bot = len(counter.keys()) - (len(counter.keys()) // 10)
+    wordcloud_keywords = []
+    while counter.most_common()[0][1] > 0 and words_added < 150:
+        common_word = counter.most_common()[0][0]
+        common_count = counter.most_common()[0][1]
+        # if count > cut_top:
+        wordcloud_keywords.extend([common_word] * common_count)
+        words_added += 1
+        counter.subtract(Counter({common_word: common_count}))
+        count += 1
+    # return
+
+    # Generate word cloud
+    wordcloud = WordCloud(width=1920, height=1080, random_state=1, background_color='black', colormap='Pastel1',
+                          collocations=False, stopwords=STOPWORDS).generate(" ".join(wordcloud_keywords))
+
+    # Set figure size
+    plt.figure(figsize=(16, 9))
+    # Display image
+    plt.imshow(wordcloud)
+    # No axis details
+    plt.axis("off")
+    plt.show()
+
+
+    top_keywords = []
+    for i in range(len(sorted_values) // 4):
+        # if sorted_values[i][1] not in top_keywords:
+        top_keywords.append(sorted_values[i][1])
+
+    print(top_keywords)
